a82f1044ebe02bb4bf858ea8d0d7222ed417c2bc
amilcar <52044033+Terminal-Redux@users.noreply.github.com>
2022-05-26 09:14:58 -0500

    Editar heroes

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Heroe, Publisher } from '../../interfaces/heroe';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-agregar',
@@ -8,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    },
+  ]
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: ''
+  }
+
+  constructor(private heroesService: HeroesService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({id}) => this.heroesService.getHeroesPorId(id))
+    )
+    .subscribe(heroe => this.heroe = heroe
+    );
+  }
+
+  guardar() {
+    console.log('HEY!');
+
+    if(this.heroe.superhero.trim().length === 0) {
+      return;
+    }
+
+    if(this.heroe.id) {
+      // Actualizar
+      this.heroesService.actualizarHeroe(this.heroe)
+      .subscribe(resp => {console.log('Respuesta: ', resp)}
+      );
+    } else {
+      // Crear registro
+      this.heroesService.agregarHeroe(this.heroe)
+      .subscribe(heroe => this.router.navigate(['/heroes/editar', heroe.id])
+      );
+    }
+
   }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -24,4 +24,12 @@ export class HeroesService {
   getSugerencias(termino: string): Observable<Heroe[]> {
     return this.http.get<Heroe[]>(`${this._baseUrl}/heroes?q=${termino}&_limit=6`);
   }
+
+  agregarHeroe(heroe: Heroe): Observable<Heroe> {
+    return this.http.post<Heroe>(`${this._baseUrl}/heroes`, heroe);
+  }
+
+  actualizarHeroe(heroe: Heroe): Observable<Heroe> {
+    return this.http.put<Heroe>(`${this._baseUrl}/heroes/${heroe.id}`, heroe);
+  }
 }
